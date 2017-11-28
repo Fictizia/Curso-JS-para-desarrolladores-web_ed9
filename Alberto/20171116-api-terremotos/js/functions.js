0305e2566884dd2bde1ad6171a524d74dda40a7c
Javascript <you@example.com>
2017-11-28 21:56:55 +0100

    updated after class

--- Alberto/20171116-api-terremotos/js/functions.js
+++ Alberto/20171116-api-terremotos/js/functions.js
@@ -1,3 +1,16 @@
+const search = document.getElementById('search');
+	
+search.addEventListener('click', function(event){ //pide ejecutar la llamada a la api por callback
+		
+    event.preventDefault(); //impide que el click refresque la página, tal como haría por defecto. el event es un parámetro que hace referencia al evento mencionado como primer parámetro del addEventListener, en este caso el 'click'
+    let yearOrigin = document.getElementById('years').value;
+    let minMagnitude = document.getElementById('magnitudes').value;
+		
+    getData(yearOrigin,minMagnitude);
+});
+
+
+
 function getThisYear(){  //extrae el año actual
 	let today = new Date(); //devuelve la fecha de hoy
 	let thisYear = today.getFullYear(); //extrae el año de today
@@ -34,30 +47,14 @@ function showMagnitude(){ //muestra las opciones del select de magnitudes
 	})
 }
 
-function dataQuery(callback){ //recoge las opciones señaladas al pulsar "buscar" 
-	
-	const search = document.getElementById('search');
-	
-	search.addEventListener('mouseover', function(){ //pide ejecutar la llamada a la api por callback
-		
-		let yearOrigin = document.getElementById('years').value;
-		let minMagnitude = document.getElementById('magnitudes').value;
-		
-		callback(yearOrigin,minMagnitude);
-	})
-
-}
-
 async function getData(year,magnitude){ //hace la petición y convierte el resultado en json
 	let url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
     url += `&minmagnitude=${magnitude}&starttime=${year}0101`;
 		await console.log(url);	
-	data = await fetch(url);
-	dataJson = await data.json();
+	let data = await fetch(url);
+	let dataJson = await data.json();
 	
-	await console.log(dataJson);
-	await showData(dataJson);
-	
+    await showData(dataJson);
 }
 
 function showData(data){
@@ -75,90 +72,11 @@ function showData(data){
 		<li>MMI: ${eQuakeInEquakes.properties.mmi}</li>
 		<li><a href="${eQuakeInEquakes.properties.url}">Link al evento</li></ul>
 		`
-	})
-	
+	});
 }
 
 
 getThisYear();
 showMagnitude();
-dataQuery(getData);
 
 
-
-/*
-let buscar = document.getElementById('buscar');
-buscar.addEventListener('click',function(){
-let valor = document.getElementById('anio').value;
-alert(valor);
-})
-
-
-
-
-function collectQuery(){
-    year = document.eqForm.anio.value;
-    alert('Hola');
-    getData(year);
-}
-
-
-
-async function getData(b){
-    let url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
-    //url += '&minmagnitude=' + a;
-    url += '&starttime=' + b + '0101';
-    
-    data = await fetch(url);
-    dataJson = await data.json();
-    
-    await getEQ(dataJson);
-    
-}
-
-async function getEQ(a){
-    eQuakes = a.features;
-    
-    await showProperties(eQuakes);
-}
-
-
-/* MEDIANTE BUCLE FOR 
-async function showProperties(a){    
-    for (i=0; i<= a.length; i++){
-        document.write(`
-            <ul>
-                <li>Terremoto ${i+1}: </li>
-                <li>Magnitud: ${a[i].properties.mag}</li>
-                <li>Localización: ${a[i].properties.place}</li>
-            </ul>
-            `)
-    }
-}
-
-*/
-
-
-
-/* MEDIANTE FOR EACH */
-
-async function showProperties(a){ 
-    // tenemos un array, con objetos dentro
-    // cada elmento del array quiero pintar
-    // elemento.properties.mag
-    // elemento.properties.place
-    
-    
-    a.forEach(function(terremotoEnArray,index){
-       document.write(`
-            <ul>
-                <li>Terremoto ${index+1}: </li>
-                <li>Magnitud: ${terremotoEnArray.properties.mag}</li>
-                <li>Localización: ${terremotoEnArray.properties.place}</li>
-            </ul>
-            `)
-    })
-}
-
-
-//getData(7,20170101); 
